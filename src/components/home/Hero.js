62b01a86325956e993dd52387de779ba0031b8e2
maliukalina <l.malyuk@gmail.com>
2021-11-28 12:21:40 -0500

    @media was added

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -10,10 +10,8 @@ export default function Hero() {
 return(
   <div>
         <div className="textBlock">
-        <div className="textCell">
-          <p className="allText">Our App searches for personalized culinary vacations that promise incredible dinning options. If you love food and want to explore a new destinations, try Foodie Travel App today
-          </p>
-          </div>
+        <div className="allText">Our App searches for personalized culinary vacations that promise incredible dinning options. If you love food and want to explore a new destinations, try Foodie Travel App today
+        </div>
           <div className="textCell">
           <p className="textHeader">Find your ideal Foodie Destination in the World</p>
           <button onClick={handleGetStarted} className="button">Get started</button>
